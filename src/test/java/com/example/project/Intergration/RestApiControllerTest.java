2e3d38a67c11732f5ca9700fb072d683c861e73c
miyazawayamato <yamato8781@gmail.com>
2021-05-18 16:12:47 +0900

    test

--- src/test/java/com/example/project/Intergration/RestApiControllerTest.java
+++ src/test/java/com/example/project/Intergration/RestApiControllerTest.java
@@ -6,14 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
-// import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-// import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-// import com.example.project.Entity.Products;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
