9b912f803c5ac68e3259d554468abd20b5827813
miyazawayamato <yamato8781@gmail.com>
2021-05-28 06:37:45 +0900

    5/28

--- src/main/java/com/example/project/Repository/DeliveriesRepository.java
+++ src/main/java/com/example/project/Repository/DeliveriesRepository.java
@@ -1,9 +1,7 @@
 package com.example.project.Repository;
 
-import java.util.List;
 
 import com.example.project.Entity.Deliveries;
-import com.example.project.Form.HistoriesForm;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
